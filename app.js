a5d805c0eb60534a96c615ec2a46ee081919cc0e
Nidhi Sharma <nidhi.sharma@pmcretail.com>
2023-01-20 12:13:52 +0530

    removed comments

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-// const express = require("express");
-
-// const app = express();
-
-// app.get ('/', (req, res) => {
-//     res.send("Hello Nidhi")
-// })
-
-// app.listen(3000, () => {
-//     console.log("App listening on port 3000...");
-// })
-
 console.log("Hello Nidhi")
